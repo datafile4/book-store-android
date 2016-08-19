7bf75df266325536df6a552ed63f8c328c12d1ff
Emil Alasgarov <debianmik@gmail.com>
2016-08-19 13:33:26 +0400

    Added item layout and empty BookAdapter

---
+++ app/src/main/java/com/example/datafile4/bookstore/BookAdapter.java
@@ -0,0 +1,12 @@
+package com.example.datafile4.bookstore;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by datafile4 on 8/19/16.
+ */
+public class BookAdapter{ /*extends RecyclerView.Adapter<BookAdapter.ViewHolder>{
+//    public static class ViewHolder extends RecyclerView.ViewHolder{
+//        public
+//    } */
+}
